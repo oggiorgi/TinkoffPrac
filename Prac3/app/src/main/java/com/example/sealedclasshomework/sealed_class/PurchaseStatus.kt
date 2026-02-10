e55e64c91c443b57855830b9ab5b2de5fd160e56
Giorgi Olefirov <ranertop@gmail.com>
2026-02-10 22:23:30 +0300

    Update PurchaseStatus.kt

--- Prac3/app/src/main/java/com/example/sealedclasshomework/sealed_class/PurchaseStatus.kt
+++ Prac3/app/src/main/java/com/example/sealedclasshomework/sealed_class/PurchaseStatus.kt
@@ -1,4 +1,18 @@
 package com.example.sealedclasshomework.sealed_class
+//это при 2 варианте меняем open на sealed
+//от
+sealed class PurchaseStatus {
+//до
+    object InProgress: PurchaseStatus()
+
+    object Ready: PurchaseStatus()
+
+    data class Canceled(val reason: String): PurchaseStatus()
+}
+
+/**дефолт 1 и 3
+
+package com.example.sealedclasshomework.sealed_class
 
 open class PurchaseStatus {
 
@@ -8,3 +22,4 @@ open class PurchaseStatus {
 
     data class Canceled(val reason: String): PurchaseStatus()
 }
+ */
