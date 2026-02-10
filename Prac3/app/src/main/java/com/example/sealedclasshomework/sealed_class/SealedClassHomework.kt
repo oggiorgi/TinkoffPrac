a6f65463935ff6570d3bcbef90e7cddde30a0243
Giorgi Olefirov <ranertop@gmail.com>
2026-02-10 22:23:59 +0300

    Update SealedClassHomework.kt

--- Prac3/app/src/main/java/com/example/sealedclasshomework/sealed_class/SealedClassHomework.kt
+++ Prac3/app/src/main/java/com/example/sealedclasshomework/sealed_class/SealedClassHomework.kt
@@ -3,16 +3,42 @@ package com.example.sealedclasshomework.sealed_class
 /**
  * У нас есть метод getPurchaseStatus(), но сейчас Android Studio ругается на ошибку. Исправьте ее и запустите программу.
  * Что будет выведено в терминале?
+ * ответ: 'when' expression must be exhaustive, add necessary 'else' branch
  * Помните, что задачу можно решить несколькими способами.
  */
 fun main() {
     getPurchaseStatus(PurchaseStatus.InProgress)
 }
-
+/** Первый способ решения это как предлагает решить проблему сам компилятор через else при open class PurchaseStatus
 fun getPurchaseStatus(status: PurchaseStatus) = when (status) {
     PurchaseStatus.InProgress -> println("доставка в процессе")
     PurchaseStatus.Ready -> println("доставка готова")
     is PurchaseStatus.Canceled -> {
         println("Доставка отменена, причина: ${status.reason}")
     }
+от
+    else -> {}
+до
 }
+ */
+// Второй способ сделать класс sealed, тк в отличие от open в нём не обязательно писать else. При sealed class PurchaseStatus
+fun getPurchaseStatus(status: PurchaseStatus) = when (status) {
+    PurchaseStatus.InProgress -> println("доставка в процессе")
+    PurchaseStatus.Ready -> println("доставка готова")
+    is PurchaseStatus.Canceled -> {
+        println("Доставка отменена, причина: ${status.reason}")
+    }
+
+}
+
+/** Третий вариант
+fun getPurchaseStatus(status: PurchaseStatus) {
+    when (status) {
+        PurchaseStatus.InProgress -> println("доставка в процессе")
+        PurchaseStatus.Ready -> println("доставка готова")
+        is PurchaseStatus.Canceled -> {
+            println("Доставка отменена, причина: ${status.reason}")
+        }
+    }
+}
+ */
